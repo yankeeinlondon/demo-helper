1c70576ee76a36a62bc56ebe7feb3312b3cfcba7
Ken Snyder <ken@ken.net>
2015-08-13 08:00:36 +0100

    small fixes to indents

--- addon/components/display-component.js
+++ addon/components/display-component.js
@@ -1,18 +1,40 @@
 import Ember from 'ember';
+const htmlSafe = Ember.String.htmlSafe;
 const { keys, create } = Object; // jshint ignore:line
 const {computed, observer, $, A, run, on, typeOf, debug, defineProperty, get, set, inject, isEmpty} = Ember;  // jshint ignore:line
 import layout from '../templates/components/display-component';
+const ignoredProperties = ['_indent','__ember_meta__', 'as', 'componentName','_dim','_container'];
 
 export default Ember.Component.extend({
   layout: layout,
   classNames: ['display-component'],
+  classNameBindings: ['_dim:dim:normal'],
   componentName: null,
   displayEmpty: false,
+  _hasContainer: computed(function(){
+    return this.get('_container') ? true : false;
+  }),
+  _indent: 0,
+  _indentSpacing: computed('_indent', function() {
+    const indent = this.get('_indent');
+    let indentText =[];
+    let index = 1;
+    while (indentText.length < indent) {
+      indentText.push(`<span class="indent indent-${index}">&nbsp;&nbsp;&nbsp;&nbsp;</span>`);
+      index++;
+    }
+    return htmlSafe(indentText.join(''));
+  }),
+  _hasVisibleProperties: computed('_attrs','propertyMutex', function(){
+    return this.get('_props').filter(item => {
+      return item.type !== 'undefined';
+    }).length > 0;
+  }),
   _attrs: computed({
     get() {
       const attrs = Object.keys(this.get('attrs'));
       const attributes = attrs.filter(item => {
-        return new Set(['__ember_meta__', 'as', 'componentName']).has(item) ? false : true;
+        return new Set(ignoredProperties).has(item) ? false : true;
       });
       // add observers to each
       attributes.map(item => {
@@ -32,8 +54,12 @@ export default Ember.Component.extend({
   getProps() {
     const attributes = this.get('_attrs');
     return new A(attributes.map(item => {
-      const value = this.get(item);
-      const type = typeOf(value);
+      let value = this.get(item);
+      let type = typeOf(value);
+      if(type === 'string' && value.substr(0,1) === ':') {
+        value = value.substr(1);
+        type = 'bound-variable';
+      }
       const quoteMark = type === 'string' ? '\'' : '';
       const display = !this.get('displayEmpty') && isEmpty(value) ? false : true;
       return {prop: item, value: value, type: type, quoteMark: quoteMark, display: display};
