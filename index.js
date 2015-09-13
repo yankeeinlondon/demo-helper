c726931b6797402958ef3b791be7d1c32a5350fb
Ken Snyder <ken@ken.net>
2015-09-13 12:11:40 +0100

    auto-insert a Ember icon for dummy testing app

--- index.js
+++ index.js
@@ -5,5 +5,15 @@ module.exports = {
   name: 'demo-helper',
   included: function(app) {
     app.import('vendor/demo-helper/demo-helper.css');
+  },
+  contentFor: function(type, config) {
+    switch(type) {
+      case 'head':
+        console.log('config object: %o', config);
+        return '<link href="/vendor/demo-helper/emberjs.png" rel="icon" type="image/png" />';
+
+      default:
+        return '';
+    }
   }
 };
