1cbbe221a7fb0b19633e2c6410c15593a8678bee
Ken  Snyder <ken@ken.net>
2015-09-13 16:13:34 +0100

    started app-page component

---
+++ app/components/app-page.js
@@ -0,0 +1,1 @@
+export { default } from 'demo-helper/components/app-page';
---
+++ tests/integration/components/app-page-test.js
@@ -0,0 +1,26 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('app-page', 'Integration | Component | app page', {
+  integration: true
+});
+
+test('it renders', function(assert) {
+  assert.expect(2);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+
+  this.render(hbs`{{app-page}}`);
+
+  assert.equal(this.$().text().trim(), '');
+
+  // Template block usage:
+  this.render(hbs`
+    {{#app-page}}
+      template block text
+    {{/app-page}}
+  `);
+
+  assert.equal(this.$().text().trim(), 'template block text');
+});
