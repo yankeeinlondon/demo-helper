f7175c595344ac32d6790caa608deacc4087cf50
Ken Snyder <ken@ken.net>
2015-11-11 21:48:40 +0000

    added json helper

---
+++ app/helpers/json.js
@@ -0,0 +1,1 @@
+export { default, json } from 'demo-helper/helpers/json';
---
+++ tests/unit/helpers/json-test.js
@@ -0,0 +1,10 @@
+import { json } from '../../../helpers/json';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | json');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  var result = json(42);
+  assert.ok(result);
+});
