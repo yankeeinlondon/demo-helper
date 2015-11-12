e422e0f9d3eeba1753a376b9cb8865c3132a245c
Ken Snyder <ken@ken.net>
2015-11-12 21:56:03 +0000

    lazy loading of badges

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -17,7 +17,7 @@ module.exports = function(environment) {
       'script-src': "'self'",
       'font-src': "'self' http://fonts.gstatic.com http://fonts.googleapis.com",
       'connect-src': "'self'",
-      'img-src': "'self' https://npmjs.org https://npmjs.com https://badge.fury.io https://travis-ci.org https://codeclimate.com",
+      'img-src': "'self' https://npmjs.org https://npmjs.com https://badge.fury.io https://travis-ci.org https://codeclimate.com https://api.travis-ci.org",
       'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com",
       'media-src': "'self'"
     },
