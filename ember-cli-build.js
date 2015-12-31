7b0033a673507519c7b8ffdd1233a94a53fc8b72
Ken Snyder <ken@ken.net>
2015-12-31 13:46:55 +0000

    config change

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -3,16 +3,9 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
   var app = new EmberAddon(defaults, {
-    babel: {
-    }
+    babel: { includePolyfill: true }
   });
-  app.options.babel = { includePolyfill: true };
-  /*
-    This build file specifes the options for the dummy test app of this
-    addon, located in `/tests/dummy`
-    This build file does *not* influence how the addon or the app using it
-    behave. You most likely want to be modifying `./index.js` or app's build file
-  */
+
   app.import('bower_components/bootstrap/dist/css/bootstrap.css');
   app.import('bower_components/bootstrap/js/tooltip.js');
   app.import('bower_components/fontawesome/css/font-awesome.css');
